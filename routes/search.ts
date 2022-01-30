b8eeea7d3a000eb216df4c1ba789bc723beef2e6
pachniar <01130999@pw.edu.pl>
2022-01-30 16:24:44 +0100

    product fix

--- routes/search.ts
+++ routes/search.ts
@@ -13,11 +13,7 @@ module.exports = function searchProducts () {
   return (req, res, next) => {
     let criteria = req.query.q === 'undefined' ? '' : req.query.q || ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%:status%' OR description LIKE '%:status%') AND deletedAt IS NULL) ORDER BY name` ,
-    {
-      replacements: { status: criteria },
-      type: QueryTypes.SELECT
-    }) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]) => {
         const dataString = JSON.stringify(products)
         if (utils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
